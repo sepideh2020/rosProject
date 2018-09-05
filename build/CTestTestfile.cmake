d321ede094b980d45a53f1905e0588b5ee70ed68
sepideh2020 <sepideniktab2020@gmail.com>
2018-09-05 10:50:56 +0430

    base

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sepideh/Desktop/rosproject/src
+# Build directory: /home/sepideh/Desktop/rosproject/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(mypack)
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/sepideh/Desktop/rosproject/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/sepideh/Desktop/rosproject/devel;/opt/ros/kinetic".split(';') if "/home/sepideh/Desktop/rosproject/devel;/opt/ros/kinetic" != "" else []
