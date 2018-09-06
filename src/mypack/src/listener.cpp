8d98dcdb67f80a5124fd4ac6a86a212b6665c59e
sepideh2020 <sepideniktab2020@gmail.com>
2018-09-06 11:56:05 +0430

    talker removed and merged with listener

--- src/mypack/src/listener.cpp
+++ src/mypack/src/listener.cpp
@@ -1,10 +1,9 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "turtlesim/Pose.h"
+#include "geometry_msgs/Twist.h"
+#include <sstream>
 
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
 void chatterCallback(const turtlesim::Pose::ConstPtr& msg)
 {
   ROS_INFO("I heard: [%f],[%f],[%f],[%f],[%f] \n", msg->x, msg->y, msg->theta, msg->linear_velocity, msg->angular_velocity);
@@ -12,49 +11,32 @@ void chatterCallback(const turtlesim::Pose::ConstPtr& msg)
 
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+
   ros::init(argc, argv, "listener");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
   ros::NodeHandle n;
-
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
   ros::Subscriber sub = n.subscribe("turtle1/pose", 1000,chatterCallback);
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
-  ros::spin();
+
+//  ros::spin();
+
+
+  ros::init(argc, argv, "talker");
+
+  ros::Publisher chatter_pub = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
+  ros::Rate loop_rate(10);
+  int count = 0;
+  while (ros::ok())
+  {
+    geometry_msgs::Twist msg;
+    msg.linear.x=2;
+    msg.angular.y=2;
+    chatter_pub.publish(msg);
+    ros::spinOnce();
+    loop_rate.sleep();
+    ++count;
+  }
+
 
   return 0;
 }
-
